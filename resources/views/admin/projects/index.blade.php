d3bcebb3e32a343d0fa0714c3bdc84bbf15a7142
nicolaazonta <nicolazonta114@gmail.com>
2023-06-11 23:46:05 +0200

    vara che ora che ze PUSH

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -8,73 +8,52 @@
         </div>
         <ul class="list-group list-group-flush">
             @foreach ($types as $type)
-                <li class="list-group-item bg-transparent text-uppercase fw-bolder d-flex align-items-center justify-content-between">
+                <li
+                    class="list-group-item bg-transparent text-uppercase fw-bolder d-flex align-items-center justify-content-between">
                     {{ $type->name }}
                     <div>
-                        <a href=""><i class="fa-solid fa-pencil fa-2x text-dark me-1" aria-hidden="true"></i></a>{{-- edit --}}
+                        <a href=""><i class="fa-solid fa-pencil fa-2x text-dark me-1"
+                                aria-hidden="true"></i></a>{{-- edit --}}
 
 
-                        <a data-bs-toggle="modal"
-                        data-bs-target="#modal-{{ $type->id }}" href=""><i class="fa-solid fa-trash fa-2x text-dark" aria-hidden="true"></i></a>{{-- delete --}}
+                        <a data-bs-toggle="modal" data-bs-target="#modal-{{ $type->id }}" href=""><i
+                                class="fa-solid fa-trash fa-2x text-dark" aria-hidden="true"></i></a>{{-- delete --}}
                     </div>
 
-                 {{--    <button type="button" class="btn btn-danger m-1" data-bs-toggle="modal"
-                    data-bs-target="#modal-{{ $project->id }}">
-                    delete
-                </button> --}}
+                    <!-- Modal Body -->
+                    <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+                    <div class="modal fade" id="modal-{{ $type->id }}" tabindex="-1" data-bs-backdrop="static"
+                        data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="modalTitleId">attention</h5>
+                                </div>
+                                <div class="modal-body">
+                                    are u sure to delete this type?
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
 
-                <!-- Modal Body -->
-                <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
-                <div class="modal fade" id="modal-{{ $type->id }}" tabindex="-1"
-                    data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
-                    aria-labelledby="modalTitleId" aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
-                        role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="modalTitleId">attention</h5>
-                            </div>
-                            <div class="modal-body">
-                                are u sure to delete this type?
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary"
-                                    data-bs-dismiss="modal">Close</button>
+                                    <form action="{{ route('admin.types.destroy', $type->id) }}" method="post">
+                                        @csrf
+                                        @method('DELETE')
 
-                                <form action="{{ route('admin.types.destroy', $type->id) }}"
-                                    method="post">
-                                    @csrf
-                                    @method('DELETE')
-
-                                    <button type="submit" class="btn btn-primary">Delete</button>
-                                </form>
+                                        <button type="submit" class="btn btn-primary">Delete</button>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
                 </li>
             @endforeach
-            <div class="input-group">
-                <input type="text" class="form-control" placeholder="new type" aria-label="Recipient's username"
+            <form class="input-group" action="{{ route('admin.types.store') }}" method="post">
+                @csrf
+                <input   type="text" class="form-control" placeholder="new type" aria-label="Recipient's username"
                     aria-describedby="button-addon2">
-                <button class="btn btn-outline-dark" type="button" id="button-addon2">add</button>
-            </div>
+                <button class="btn btn-outline-dark" type="submit" id="button-addon2">add</button>
+            </form>
         </ul>
     </div>
 
